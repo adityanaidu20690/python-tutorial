23c6082e2a0709a4981934b0f48844d239decd3b
adityanaidu20690 <adevaws@gmail.com>
2024-11-08 14:31:08 +0000

    updated the location of img file

--- test-project-pythod-database/project2.py
+++ test-project-pythod-database/project2.py
@@ -2,7 +2,7 @@ from flask import Flask , render_template
 app = Flask(__name__)
 @app.route ("/")
 def ace():
-    return render_template("index.html")
+    return render_template("test.html")
 
 @app.route ("/bootstrap")
 def addy():
